f1615e0bde0a1b4b545ea6ed6096c2fb12747722
Taras Novak <taras.novak@randomfractals.com>
2016-10-03 11:59:48 -0500

    added SalesOrderDetails cs stub

---
+++ src/AdventureWorks/AdventureWorks/Models/SalesOrderDetails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdventureWorks.Models.SalesOrder
+{
+
+    public class SalesOrderDetails
+    {
+        public int SalesOrderID { get; set; }
+
+        public int SalesOrderDetailID { get; set; }
+
+        public int OrderQty { get; set; }
+
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        [Display(Name = "Unit ($)")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal UnitPrice { get; set; }
+
+        [Display(Name = "UP Discount ($)")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal UnitPriceDiscount { get; set; }
+
+        [Display(Name = "Line Total ($)")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal LineTotal { get; set; }
+
+        [Display(Name = "List Price ($)")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal ListPrice { get; set; }
+
+    }
+}
