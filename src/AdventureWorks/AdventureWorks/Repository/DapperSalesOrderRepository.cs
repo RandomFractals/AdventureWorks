473f00533beb9e2e0a2786a8ed82ecc7d45e7028
Taras Novak <taras.novak@randomfractals.com>
2016-10-03 12:12:55 -0500

    created dapper sales orders repository stub

---
+++ src/AdventureWorks/AdventureWorks/Repository/DapperSalesOrderRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AdventureWorks.Models.SalesOrder;
+
+namespace AdventureWorks.Repository
+{
+    public class DapperSalesOrdersRepository: ISalesOrdersRepository
+	{
+
+        // TODO: add query and pagination params
+        public List<SalesOrder> GetSalesOrders()
+        {
+            return null;
+        }
+
+        public List<SalesOrderDetails> GetSalesOrderDetails(int orderId)
+        {
+            return null; 
+        }
+
+    }
+}
