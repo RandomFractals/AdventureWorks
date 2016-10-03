02357835623e34ddab11ec6222cd3532aba97a98
Taras Novak <taras.novak@randomfractals.com>
2016-10-03 12:30:22 -0500

    added quick and dirty get sales order dapper repo impl.

--- src/AdventureWorks/AdventureWorks/Repository/DapperSalesOrderRepository.cs
+++ src/AdventureWorks/AdventureWorks/Repository/DapperSalesOrderRepository.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Dapper;
 
 using AdventureWorks.Models.SalesOrder;
 
@@ -10,12 +15,29 @@ namespace AdventureWorks.Repository
 {
     public class DapperSalesOrdersRepository: ISalesOrdersRepository
 	{
+        private SqlConnection GetConnection()
+        {
+            string sqlConString = ConfigurationManager.ConnectionStrings["SqlConn"].ToString();
+            return new SqlConnection(sqlConString);
+        }
 
         // TODO: add query and pagination params
         public List<SalesOrder> GetSalesOrders()
         {
-            return null;
+            try
+            {
+                SqlConnection connect = this.GetConnection();
+                IList<SalesOrder> orderList = SqlMapper.Query<SalesOrder>(
+                    connect, "SalesGetSalesOrders").ToList();
+                connect.Close();
+                return orderList.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
+
 
         public List<SalesOrderDetails> GetSalesOrderDetails(int orderId)
         {
