3af9faa08d08fe8452f0e53c709c58d49371cdc1
Taras Novak <taras.novak@randomfractals.com>
2016-10-03 16:21:54 -0500

    enriched sales order data stored proc

--- data/procs/Sales.GetSalesOrders.sql
+++ data/procs/Sales.GetSalesOrders.sql
@@ -14,10 +14,49 @@ GO
 CREATE PROCEDURE [dbo].[SalesGetSalesOrders]
 AS
 BEGIN
-	SELECT TOP (20) SalesOrderID, OrderDate, DueDate, ShipDate, Status, 
-		SalesOrderNumber, PurchaseOrderNumber, AccountNumber, 
-    CustomerID, ShipToAddressID, ShipMethodID, CurrencyRateID,
-		SubTotal, TaxAmt, Freight, TotalDue, ModifiedDate
-	FROM Sales.SalesOrderHeader
+SELECT TOP (20) Orders.SalesOrderID, 
+		Orders.OrderDate, 
+		Orders.DueDate, 
+		Orders.ShipDate, 
+		Orders.Status, 
+		Orders.SalesOrderNumber, 
+		Orders.PurchaseOrderNumber, 
+		Orders.AccountNumber, 
+		Orders.CustomerID, 
+		Customer.PersonID,
+		Person.FirstName,
+		Person.LastName,
+		Orders.ShipToAddressID, 
+		ShipToAddress.AddressLine1,
+		ShipToAddress.AddressLine2,
+		ShipToAddress.City,
+		ShipToAddress.StateProvinceID,
+		ProvinceCountryRegion.StateProvinceCode,
+		ProvinceCountryRegion.StateProvinceName,
+		ProvinceCountryRegion.CountryRegionCode,
+		ProvinceCountryRegion.CountryRegionName,
+		ShipToAddress.PostalCode,
+		Orders.ShipMethodID, 
+		ShipMethod.[Name] AS ShipMethodName,
+		ShipMethod.ShipBase,
+		ShipMethod.ShipRate,
+		Orders.CurrencyRateID, 
+		Orders.SubTotal, 
+		Orders.TaxAmt, 
+		Orders.Freight, 
+		Orders.TotalDue, 
+		Orders.rowguid, 
+		Orders.ModifiedDate
+	FROM Sales.SalesOrderHeader AS Orders
+		INNER JOIN Sales.Customer AS Customer
+		ON Orders.CustomerID = Customer.CustomerID
+		INNER JOIN Person.Person AS Person
+		ON Customer.PersonID = Person.BusinessEntityID
+		INNER JOIN Person.[Address] As ShipToAddress
+		ON Orders.ShipToAddressID = ShipToAddress.AddressID
+		INNER JOIN Person.vStateProvinceCountryRegion as ProvinceCountryRegion
+		ON ShipToAddress.StateProvinceID = ProvinceCountryRegion.StateProvinceID
+		INNER JOIN Purchasing.ShipMethod AS ShipMethod
+		ON Orders.ShipMethodID = ShipMethod.ShipMethodID
 END
 GO
