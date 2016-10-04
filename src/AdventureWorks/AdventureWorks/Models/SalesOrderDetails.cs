b0729116241629f4a641a3f5bbc2bcfe61275e45
Taras Novak <taras.novak@randomfractals.com>
2016-10-04 11:29:13 -0500

    SalesOrderDetails.cs display labels update and props reorg for a decent SOD list view display

--- src/AdventureWorks/AdventureWorks/Models/SalesOrderDetails.cs
+++ src/AdventureWorks/AdventureWorks/Models/SalesOrderDetails.cs
@@ -11,27 +11,28 @@ namespace AdventureWorks.Models
 
         public int SalesOrderDetailID { get; set; }
 
-        public int OrderQty { get; set; }
-
         public int ProductID { get; set; }
 
+        [Display(Name = "product")]
         public string ProductName { get; set; }
 
-        [Display(Name = "Unit ($)")]
-        [DisplayFormat(DataFormatString = "{0:C}")]
-        public decimal UnitPrice { get; set; }
+        [Display(Name = "quantity")]
+        public int OrderQty { get; set; }
 
-        [Display(Name = "UP Discount ($)")]
-        [DisplayFormat(DataFormatString = "{0:C}")]
-        public decimal UnitPriceDiscount { get; set; }
-
-        [Display(Name = "Line Total ($)")]
-        [DisplayFormat(DataFormatString = "{0:C}")]
-        public decimal LineTotal { get; set; }
-
-        [Display(Name = "List Price ($)")]
+        [Display(Name = "list price")]
         [DisplayFormat(DataFormatString = "{0:C}")]
         public decimal ListPrice { get; set; }
 
+        [Display(Name = "unit price")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal UnitPrice { get; set; }
+
+        [Display(Name = "discount")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal UnitPriceDiscount { get; set; }
+
+        [Display(Name = "total")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal LineTotal { get; set; }
     }
 }
